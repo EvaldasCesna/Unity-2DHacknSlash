795674a2d35a0e22e4648caac26e1241f7a648da
Evaldas Cesna <evaldas.cesna.wrk@gmail.com>
2016-11-12 17:40:36 +0000

    1stCommit

---
+++ ProjectY4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rig;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rig = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+        float movVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(movHorizontal, movVertical);
+        rig.velocity = movement * speed;
+  
+
+    }
+}
