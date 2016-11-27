eac6bcb0be5ebd6d0977db4df4278c8af108a519
Evaldas Cesna <x00112310@gmail.com>
2016-11-27 17:50:11 +0000

    Stuff

---
+++ ProjectY4/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public int depth = -20;
+    public int x = 0;
+    public int y = 0;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = playerTransform.position + new Vector3(x, y, depth);
+        }
+    }
+
+    public void setTarget(Transform target)
+    {
+        playerTransform = target;
+    }
+}
--- ProjectY4/Assets/Scripts/PlayerMovement.cs
+++ ProjectY4/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
 
-
-public class PlayerMovement : NetworkBehaviour {
+public class PlayerMovement : NetworkBehaviour
+{
 
     public float speed;
     private Rigidbody2D rig;
- 
 
-	// Use this for initialization
-	void Start ()
+    //Animator anim;
+
+    bool isAttacking;
+
+    public override void OnStartLocalPlayer()
     {
-        rig = GetComponent<Rigidbody2D>();
-	}
+        Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
+    }
 
+    // Use this for initialization
+    void Start()
+    {
+
+
+        rig = GetComponent<Rigidbody2D>();
+     //   anim = GetComponent<Animator>();
   
+}
+
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -22,7 +35,9 @@ public class PlayerMovement : NetworkBehaviour {
         {
             return;
         }
+        
 
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
          float movHorizontal = Input.GetAxis("Horizontal");
          float movVertical = Input.GetAxis("Vertical");
 
@@ -30,6 +45,22 @@ public class PlayerMovement : NetworkBehaviour {
         Vector2 movement = new Vector2(movHorizontal, movVertical);
         rig.velocity = movement * speed;
 
+      //  isAttacking = Input.GetKeyDown(KeyCode.Space);
+
+#else
+        Vector2 movement = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+        rig.velocity = movement * speed;
+
+        //    isAttacking = CrossPlatformInputManager.GetButton("Attack");
+
+
+
+#endif
+
+        //     if (isAttacking)
+        //     {
+        //          anim.SetTrigger("Attack");
+        //    }
     }
 
 }
